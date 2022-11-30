cde46b9c6dd54f373bac5004ed9f2ce71f400748
den45235 <96654197+den45235@users.noreply.github.com>
2022-11-30 16:38:57 +0300

    передача данных внутрь любого компонента на уровлен пропсов

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React, {useState} from 'react';
 import Counter from "./components/counter";
+import ClassCounter from "./components/ClassComponent";
+import './styles/App.css'
+import PostItem from "./components/PostItem";
+
 function App() {
     const [value, setValue] = useState('text in inputs')
     // console.log(count)
     // console.log(setCount)
 
-
-
-
     return (
-    <div className="App">
-       <Counter/>
-        <Counter/>
-        <Counter/>
-    </div>
+        <div className="App">
+            <PostItem post={{id:1, title: 'Javascript', body: 'Description'}} />
+            <PostItem post={{id:2, title: 'Javascript', body: 'Description'}} />
+            <PostItem post={{id:3, title: 'Javascript', body: 'Description'}} />
+
+        </div>
+
   );
 }
 
