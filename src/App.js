9e775064cd33de96a3aef6d8ceeaf91c3d3a23d7
den45235 <96654197+den45235@users.noreply.github.com>
2022-12-01 22:35:33 +0300

    41

--- src/App.js
+++ src/App.js
@@ -3,21 +3,33 @@ import Counter from "./components/counter";
 import ClassCounter from "./components/ClassComponent";
 import './styles/App.css'
 import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
 
 function App() {
-    const [value, setValue] = useState('text in inputs')
-    // console.log(count)
-    // console.log(setCount)
+    const [posts, setPosts] = useState( [
+        {id: 1, title: 'Javascript', body: 'Description'},
+        {id: 2, title: 'Javascript 2', body: 'Description'},
+        {id: 3, title: 'Javascript 3', body: 'Description'},
+
+    ])
+
+
+    const [posts1, setPosts1] = useState( [
+        {id: 1, title: 'Javascript', body: 'Description'},
+        {id: 2, title: 'Javascript 2', body: 'Description'},
+        {id: 3, title: 'Javascript 3', body: 'Description'},
+
+    ])
 
     return (
         <div className="App">
-            <PostItem post={{id:1, title: 'Javascript', body: 'Description'}} />
-            <PostItem post={{id:2, title: 'Javascript', body: 'Description'}} />
-            <PostItem post={{id:3, title: 'Javascript', body: 'Description'}} />
+           <PostList posts={posts} title = "список постов 0"/>
+           <PostList posts={posts1} title = "список постов 1"/>
+
 
         </div>
 
-  );
+    );
 }
 
 export default App;
